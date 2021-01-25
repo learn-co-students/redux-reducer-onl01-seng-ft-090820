13d6af7099fef7b34a40dad2e52d62aa7fdf9ecb
HeartyRhombus <kaylahmg@gmail.com>
2021-01-25 11:09:55 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
 export function manageFriends(state, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return {
+                ...state,
+                friends: [...state.friends, action.friend]
+            };
+        case 'REMOVE_FRIEND':
+            const filteredFriends = state.friends.filter(friend => action.id !== friend.id )
+            
+            return {
+                ...state,
+                friends: filteredFriends
+            };
+        default:
+            return state;
+    }
+
 }
